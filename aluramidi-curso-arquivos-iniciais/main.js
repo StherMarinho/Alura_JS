6c087c2f10f0421db111f67efbf5f37681365168
Sther Marinho <sthermarinhobrito@gmail.com>
2026-01-27 13:30:43 -0300

    Add JS

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,1 +1,22 @@
-document.querySelector('.tecla_pom')
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador]; 
+
+    const instrumento = tecla.classList[1];
+
+    
+    const idAudio = `#som_${instrumento}`; //template string
+    //console.log(idAudio);
+
+    tecla.onclick = function() { //função anônima - é uma função que serve para ser atribuída a uma variável ou a um evento, úteis quando queremos executar alguma coisa uma única vez ou em apenas um lugar
+        tocaSom(idAudio)
+    };
+
+    //console.log(contador);
+}
