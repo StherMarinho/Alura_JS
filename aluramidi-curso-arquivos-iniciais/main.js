8fb0999cb2282031efb3ac8f10cfa66c914f4fff
Sther Marinho <sthermarinhobrito@gmail.com>
2026-01-27 23:07:24 -0300

    Add JS

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,5 +1,12 @@
-function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if(elemento != null && elemento.localName === 'audio') { //pode omitir o != null, pois o js já entende que se for null, é false
+        elemento.play();
+    }
+    else {
+        console.log('Elemento não é um áudio ou seletor inválido');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -17,6 +24,17 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     tecla.onclick = function() { //função anônima - é uma função que serve para ser atribuída a uma variável ou a um evento, úteis quando queremos executar alguma coisa uma única vez ou em apenas um lugar
         tocaSom(idAudio)
     };
+
+    tecla.onkeydown = function(evento) { //evento é um paramentro do tipo objeto que já é fornecido pelo próprio navegador ao usar o onkeydown
+        //console.log(evento.code);
+        if(evento.code ==='Enter' || evento.code === 'Space'){
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup  = function() {
+        tecla.classList.remove('ativa'); 
+    }
 
     //console.log(contador);
 }
